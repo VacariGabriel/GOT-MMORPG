c4af4c023f7ffbee189ee76a3e6a69206733b745
Gabriel Vacari <gabriel_vacari@outlook.com>
2019-06-13 20:25:32 -0300

    atualizando moedas

--- app/models/jogoDAO.js
+++ app/models/jogoDAO.js
@@ -55,8 +55,31 @@ JogoDAO.prototype.acao = function(acao) {
 
             collection.insert(acao);
             
-            mongoclient.close();
         });
+        
+        mongoclient.collection("jogo", (error, collection) => {
+            
+            let moedas = null;
+            
+            switch(parseInt(acao.acao)) {
+                case 1: 
+                moedas = -2 * acao.quantidade; break;
+                case 2: 
+                moedas = -3 * acao.quantidade; break;
+                case 3: 
+                moedas = -1 * acao.quantidade; break;
+                case 4: 
+                moedas = -1 * acao.quantidade; break;
+            } 
+            
+            collection.update(
+                { usuario: acao.usuario },
+                { $inc: { moeda: moedas }}
+                );
+                
+                mongoclient.close();
+        });
+            
     });
 }
 
